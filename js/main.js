512929ac5afbf8a84d1165ff5da3a7b45bafbac7
muladamai <muladamai@yahoo.com>
2015-11-23 08:42:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ $ = jQuery;
 	$(document).ready(function(){
 	$('p[destinations]').each(function() {
 	var text = $(this).attr('destinations');
-	console.log(text)						
-	//$('#Destinationinline').val(text);
-	$('#Destinationinline').append("<h1>"+text+"</h1>");
-	});			
+	console.log(text)		
+	$('#Destinationinline').append("<h1>"+text+"</h1>");});			
 });
