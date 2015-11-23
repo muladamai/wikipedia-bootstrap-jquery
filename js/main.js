36228ee2e77ff95c897ab3100a0f8567e209fb09
muladamai <muladamai@yahoo.com>
2015-11-23 09:02:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ $ = jQuery;
 	console.log(text)		
 	$('#Destinationinline').append("<p>"+text+"</p>");});
 	//$('a[hreflang]').each(function() {
-	$('#langselect')each(function(){
+	$('#langselect').each(function(){
 	var lang = $(this).attr('hreflang');
 	console.log(lang)});
 });
 $(".dropdown-menu li a").click(function(){
   var selText = $(this).text();
   $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-  $(this).parents('.btn-group').find('.dropdown-toggle').attr("width", "500");
+  $(this).parents('.btn-group').find('.dropdown-toggle').attr("hreflang", selText);
 });
